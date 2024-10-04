f7fd76fb2b738656b15b72ab340502aee87e8b8f
manojbp <manojbp34@gmail.com>
2024-10-04 22:48:07 +0530

    new change

---
+++ start_container.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+set -e
+
+# Pull the Docker image from Docker Hub
+docker pull manojbp609/simple-feedback-app
+
+# Run the Docker image as a container
+docker run -d -p 5000:5000 manojbp609/simple-feedback-app
+
+
